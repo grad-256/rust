e70829bc912d947ed4cf61c5576cd4ac2e30d91e
grant <miyaudi13937@gmail.com>
2022-02-21 22:40:32 +0900

    メモリ管理について

--- src/vars.rs
+++ src/vars.rs
@@ -54,4 +54,44 @@ pub fn run() {
     let a1 = [1, 2, 3, 4, 5]; // スタック
     let a2 = [0; 10];
     println!("{:?} {:?} {} {}", a1, a2, a1[2], a1[3]);
+
+    // 文字列スライスは静的領域に格納される ポインタとレングスがある
+    // 参照を意識する
+    // なぜ所有にならないのか？
+    // 静的領域に格納されるとそもそも解放する必要がない
+    // ストリング型か作った場合、所有権はString型から移動しない
+    // 所有権を移動させずに参照する権利だけ貸し出す。（借用）
+    let s1 = "hello Rust"; // 文字列スライス
+    println!("Stack address of y is: {:p}", &s1); // 格納されているアドレスをポインタ形式で表示
+    println!("Stack memory address of y is: {:?}", s1.as_ptr()); // 静的格納領域に紐付いたアドレスの値を取得
+    println!("Len of s1 is: {}", s1.len()); // スタック内の値を取得
+
+    // ストリングはヒープメモリに格納されている
+    // ヒープとは動的に確保と解放ができるメモリ領域のこと データの仮置き場や、臨時の作業台
+    // データ所有 = メモリ解放(drop)をする責任を負う
+    // 所有を意識する
+    // ポインタとレングスとキャパシティーがある
+    let mut s1 = String::from("hello");
+    let mut s2 = String::from("helloworld");
+    println!("Stack address of s1 is: {:p}", &s1); //
+    println!("Stack address of s2 is: {:p}", &s2); //
+    println!("Stack memory address of y is: {:?}", s1.as_ptr());
+    println!("Stack memory address of y is: {:?}", s2.as_ptr());
+    println!("Len of s1 is: {}", s1.len());
+    println!("Len of s2 is: {}", s2.len());
+    println!("Capacity of s1 is: {}", s1.capacity());
+    println!("Capacity of s2 is: {}", s2.capacity());
+    s1.push_str("_new1"); // 文字追加
+    s2.push_str("_new2"); // 文字追加
+    println!("Len of s1 is: {}", s1.len());
+    println!("Len of s2 is: {}", s2.len());
+    println!("Capacity of s1 is: {}", s1.capacity());
+    println!("Capacity of s2 is: {}", s2.capacity());
+    println!("{} {}", s1, s2);
+
+    // 所有権を導入する理由
+    // 二重開放エラー回避のために導入されている
+    // どうゆう時？
+    // メモリを二重に開放してしまうことを言います。
+    // 所有者だけがヒープメモリ確保されたメモリを解放できる
 }
