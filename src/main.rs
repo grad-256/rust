510f5faf7161d5ce38c1240458c030d9a899d447
grant <miyaudi13937@gmail.com>
2022-02-23 17:41:01 +0900

    動かす対象ファイル変更

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-mod vars;
+// mod stack_heap;
+mod ownership;
+// mod vars;
 
 fn main() {
         // println!("Hello, world!");
-        vars::run();
+        // vars::run();
         // vars::sub_a::func_a_run();
         // vars::sub_b::func_b_run();
+
+        // stack_heap::run()
+        ownership::run()
 }
